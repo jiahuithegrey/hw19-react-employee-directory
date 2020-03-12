2dd07d4985fb56d2cbfdc58444472bd756f8077b
Jiahui Wang <wjh4289042@hotmail.com>
2020-03-12 15:11:26 -0700

    finish searchBar component

--- src/components/SeachBar.js
+++ src/components/SeachBar.js
@@ -1,32 +1,49 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
-import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
 import Button from "react-bootstrap/Button";
 
-function SeachBar(){
-<Form>
-    <Form.Label>Seach Term</Form.Label>
-    <Form.Control type="text" placeholder="Name, gender, etc" />
+function SeachBar(props) {
+  const { handleSearch } = props;
 
-    <Dropdown>
-        <Dropdown.Toggle variant="success" id="dropdown-basic">
-            First Name
-        </Dropdown.Toggle>
-        <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Last Name</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Email</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Gender</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Phone Number</Dropdown.Item>
-        </Dropdown.Menu>
-    </Dropdown>
+  function getValue() {
+    let searchTerm = document
+      .getElementById("search-term")
+      .value()
+      .trim()
+      .toLowerCase();
+    let searchDropDown = document.getElementById("search-dropdown");
+    let searchCategory =
+      searchDropDown.options[(searchDropDown, selectedIndex)].value;
+    return { searchTerm, searchCategory };
+  }
 
-    <Button variant="primary" type="submit">Search</Button>
-</Form>
+  return (
+    <Form>
+      <Form.Label>Seach Term</Form.Label>
+      <Form.Control
+        type="text"
+        placeholder="Name, gender, etc"
+        id="search-term"
+      />
+
+      <DropdownButton id="search-dropdown" title="First Name">
+        <Dropdown.Item href="#/action-1">First Name</Dropdown.Item>
+        <Dropdown.Item href="#/action-1">Last Name</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Email</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Gender</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Phone Number</Dropdown.Item>
+      </DropdownButton>
+
+      <Button
+        variant="primary"
+        type="submit"
+        onClick={() => handleSearch(getValue())}
+      >
+        Search
+      </Button>
+    </Form>
+  );
 }
-
-return(
-
-
-)
 
 export default SeachBar;
